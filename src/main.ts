dbc8cccf16f49ec07597a2fab33a1ec54f9016be
Paulo Ricardo <paulorsscontato@hotmail.com>
2023-12-07 17:40:35 -0300

    chore: configure cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('/api/v1', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
+  const corsOptions = {
+    origin: 'localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
+  await app.listen(3001);
 }
 
 bootstrap();
